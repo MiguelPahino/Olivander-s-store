6c518473344237ef5851a0c17f2473be709761d6
MiguelPahino <mpahinogutierrez@gmail.com>
2026-02-28 19:01:14 +0100

    feat(main): :sparkles: Añadir el main de OllivanderShop

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from olivander-s-store!")
+from src.types import OllivanderShop, NormalItem, Sulfuras, Conjured, Backstage
 
+items = [
+    NormalItem("Iron Sword", 10, 30),
+    NormalItem("Leather Armor", 5, 20),
+    Sulfuras(),
+    Conjured("Conjured Mana Cake", 3, 6),
+    Backstage("Backstage pass to a TAFKAL80ETC concert", 15, 20),
+]
 
-if __name__ == "__main__":
-    main()
+shop = OllivanderShop()
+
+for item in items:
+    shop.addItem(item)
+
+shop.nextDay()
